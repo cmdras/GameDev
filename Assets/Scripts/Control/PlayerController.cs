4ce45f2623f92fc8c7fb9cc073f272e426b7fab1
chris <cmdras94@gmail.com>
2019-07-14 18:15:53 +0200

    First commit

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RPG.Movement;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PlayerController : MonoBehaviour
+    {
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                MoveToCursor();
+            }
+        }
+
+        public void MoveToCursor()
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            bool hasHit = Physics.Raycast(ray, out hitInfo);
+            if (hasHit)
+            {
+                GetComponent<Mover>().MoveTo(hitInfo.point);
+            }
+        }
+    }
+
+}
